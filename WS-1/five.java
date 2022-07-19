db30fde94916c3dc744ac3cf4c2de187c7f77003
thebluejay <76274843+jddark62@users.noreply.github.com>
2022-07-19 04:32:26 +0000

    ws2 start, five done ws1

--- WS-1/five.java
+++ WS-1/five.java
@@ -9,6 +9,49 @@ import java.util.Scanner;
  */
 public class five {
     public static void main(String[] args) {
-            
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your astrological sign: ");
+        String sign = input.nextLine();
+        switch (sign) {
+            case "Aries":
+                System.out.println("You will be successful in your work.");
+                break;
+            case "Taurus":
+                System.out.println("You will be successful in your work.");
+                break;
+            case "Gemini":
+                System.out.println("You will be successful in your work.");
+                break;
+            case "Cancer":
+                System.out.println("You will be successful in your work.");
+                break;
+            case "Leo":
+                System.out.println("You will be successful in your work.");
+                break;
+            case "Virgo":
+                System.out.println("You will be successful in your work.");
+                break;
+            case "Libra":
+                System.out.println("You will be successful in your work.");
+                break;
+            case "Scorpio":
+                System.out.println("You will be successful in your work.");
+                break;
+            case "Sagittarius":
+                System.out.println("You will be successful in your work.");
+                break;
+            case "Capricorn":
+                System.out.println("You will be successful in your work.");
+                break;
+            case "Aquarius":
+                System.out.println("You will be successful in your work.");
+                break;
+            case "Pisces":
+                System.out.println("You will be successful in your work.");
+                break;
+            default:
+                System.out.println("You will be successful in your work.");
+                break;
+        }
     }
 }
---
+++ WS-2/one.java
@@ -0,0 +1,19 @@
+/*
+ * Implement a class Employee. 
+ * An employee has a name (a string) and a salary (a double). 
+ * Write a default constructor, 
+ * a constructor with two parameters (name and salary), and 
+ * methods to return the name and salary. 
+ * Write a small program that tests your class.
+ */
+
+ /**
+  * one
+  */
+ 
+ public class one {
+ 
+    public static void main(String[] args) {
+        
+    }
+ }
